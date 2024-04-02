78e150b1a5ea9c1ff04256c6f2b4f70dd73fa228
TEUNGKU ZULKIFLI <120408238+TEUNGKU-ZULKIFLI@users.noreply.github.com>
2024-04-02 18:56:13 +0000

    mengubah bio & title, hyperlink

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "TEUNGKU ZULKIFLI",
+  title: "STUDENT",
+  email: "teungkuzulkifli08@gmail.com",
+  gitHub: "TEUNGKU-ZULKIFLI",
+  instagram: "",
+  linkedIn: "teungku-zulkifli-3953612b7",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "@TEUNGKU-ZULKIFLI?si=RqtpQWzI-sGlvx59",
 };
 
 const primaryColor = "#4E567E";
