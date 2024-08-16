7de9440e472a09c3da1553912f24997abcea9540
TEUNGKU ZULKIFLI <120408238+TEUNGKU-ZULKIFLI@users.noreply.github.com>
2024-08-16 15:53:23 +0000

    mengikuti commit panduan 1-3

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/codingannnnnn.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "coding";
 
 /**
  * Sort description that expands on your title on the Home component.
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/teungku no bg.png";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Teungku Zulkifli";
 
 /**
  * Project list
