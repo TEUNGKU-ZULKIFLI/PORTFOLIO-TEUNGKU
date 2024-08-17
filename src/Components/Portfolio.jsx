51a2ce12361d90a4d0fc24a90247f72e66571f58
TEUNGKU ZULKIFLI <120408238+TEUNGKU-ZULKIFLI@users.noreply.github.com>
2024-08-17 09:00:37 +0000

    taambah foto dan sedikit script ganti photo

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/teungku no bg.png";
+import image from "../images/profil.png";
 
 const imageAltText = "Teungku Zulkifli";
 
